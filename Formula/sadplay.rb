9755f427659f8e34451b81697deb72fe46a463ba
Marco Confalonieri <marco.confalonieri@elits.com>
2021-06-26 17:31:28 +0200

    Removed CMAKE arguments

--- Formula/sadplay.rb
+++ Formula/sadplay.rb
@@ -21,7 +21,7 @@ class Sadplay < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "gmake", ".", *std_cmake_args
+    system "gmake", ".", *std_gmake_args
   end
 
   test do
