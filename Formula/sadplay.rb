e9184eea8294fa5187edb1203721394a7081b78a
Marco Confalonieri <marco.confalonieri@elits.com>
2021-06-26 21:34:08 +0200

    Bumped version to 1.0.1

--- Formula/sadplay.rb
+++ Formula/sadplay.rb
@@ -1,10 +1,7 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sadplay < Formula
   desc "Simple SDL AdLib player"
   homepage "https://sadplay.marcoconfalonieri.it"
-  url "https://github.com/mconfalonieri/sadplay/releases/download/v1.0.0/sadplay-1.0.0.tar.gz"
+  url "https://github.com/mconfalonieri/sadplay/releases/download/v1.0.1/sadplay-1.0.1.tar.gz"
   sha256 "85a1c6cfd0ba26328a43fe60429dab05b52da45be935064f52de14c20db604ca"
   license "GPL-3.0"
 
@@ -15,8 +12,6 @@ class Sadplay < Formula
 
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
