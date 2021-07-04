1904ab8f65fa9f8c5c7efe01c544d86ee87ae4f5
Marco Confalonieri <7748015+mconfalonieri@users.noreply.github.com>
2021-07-04 22:52:23 +0200

    Changed dependency order

--- Formula/sadplay.rb
+++ Formula/sadplay.rb
@@ -6,10 +6,9 @@ class Sadplay < Formula
   license "GPL-3.0"
 
   depends_on "make" => :build
-  depends_on "sdl2"
-  depends_on "fftw"
   depends_on "adplug"
-
+  depends_on "fftw"
+  depends_on "sdl2"
 
   def install
     system "./configure", "--disable-debug",
@@ -19,16 +18,4 @@ class Sadplay < Formula
     system "gmake", "install"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sadplay`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/sadplay"
-  end
 end
